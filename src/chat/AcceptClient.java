41afa721a74d01bd9c0ff1532f7d7ae21a569268
Boussoualef Mohamed Amine <124268899+MohamedAmine4@users.noreply.github.com>
2024-03-12 11:32:31 +0100

    Update AcceptClient.java

--- src/chat/AcceptClient.java
+++ src/chat/AcceptClient.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;   
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.SocketException;
+import java.net.SocketException; 
 import java.util.List;
  
 public class AcceptClient extends Thread {
