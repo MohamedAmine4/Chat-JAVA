dd08220f737ac052c371bc542de393e44d9e1c43
Boussoualef Mohamed Amine <124268899+MohamedAmine4@users.noreply.github.com>
2024-03-12 11:32:06 +0100

    Update AcceptClient.java

--- src/chat/AcceptClient.java
+++ src/chat/AcceptClient.java
@@ -1,7 +1,7 @@
 package coursExo8Chat;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.IOException; 
 import java.io.InputStreamReader;   
 import java.io.PrintWriter;
 import java.net.Socket;
